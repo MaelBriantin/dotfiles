ce66ea5119b496042965aff0c0144dda558b08aa
MaelBriantin <mael.briantin@proton.me>
2024-05-25 22:26:03 +0200

    nvim: add lsp-config plugin and update telescope with ui-select

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "php", "typescript", "svelte"},
+      ensure_installed = {"lua", "javascript", "php", "typescript", "svelte", "markdown"},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
