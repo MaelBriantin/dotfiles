9d8d1184f43621597381776ac26fcf58ad680452
MaelBriantin <mael.briantin@proton.me>
2024-05-25 16:03:47 +0200

    nvim: add config file for each plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,20 +22,5 @@ local opts = {}
 
 require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "php", "typescript"},
-  sync_install = false,
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
 
