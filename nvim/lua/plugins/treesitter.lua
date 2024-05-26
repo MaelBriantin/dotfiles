97aa52dc3d466a58025411523c6deb3733bc882a
MaelBriantin <mael.briantin@proton.me>
2024-05-26 22:57:37 +0200

    nvim: add hyprlang to treesitter and switch php LSP to phpactor

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,18 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "php", "typescript", "svelte", "markdown"},
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "php",
+        "typescript",
+        "svelte",
+        "markdown",
+        "hyprlang"
+      },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
