8706af7ad5a3d696095cf95af815f06875f02c1b
MaelBriantin <mael.briantin@proton.me>
2024-06-05 22:13:13 +0200

    nvim: add elsint to none-ls and auto_install to treesitter

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,11 +1,16 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+        require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.phpstan,
 				null_ls.builtins.formatting.pretty_php,
 			},
 		})
