8ac268fae7a94f4a0e9e656c0a377b4931565686
MaelBriantin <mael.briantin@proton.me>
2024-06-06 16:32:24 +0200

    nvim: add keymap for formatting and reformate init.lua file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
--- vim.opt.relativenumber = true
-vim.opt.number = true
+vim.opt.relativenumber = true
+-- vim.opt.number = true
 
 local opts = {}
 
 require("vim-options")
 require("lazy").setup("plugins")
-
-
-
