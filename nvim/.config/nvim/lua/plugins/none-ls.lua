c1bc08d431a692d52933fdadbe051b0f4c9a559c
MaelBriantin <mael.briantin@proton.me>
2024-08-08 19:21:09 +0200

    nvim: add phpcsfixer and remove pretty_php

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -11,7 +11,7 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.phpstan,
-				null_ls.builtins.formatting.pretty_php,
+				null_ls.builtins.formatting.phpcsfixer,
 			},
 		})
 
